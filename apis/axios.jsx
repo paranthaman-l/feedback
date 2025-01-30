50d2fa6a292db624a6b15124e7cc0a0e2970d232
paranthaman-l <paranthamanl2004@gmail.com>
2025-01-30 20:46:03 +0530

    axios changes

--- apis/axios.jsx
+++ apis/axios.jsx
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const authApi = axios.create({
-    baseURL:"http://localhost:3000/api/auth"
+    baseURL:"https://feedback-woad-psi.vercel.app/api/auth"
 })
 const userApi = axios.create({
-    baseURL:"http://localhost:3000/api/user"
+    baseURL:"https://feedback-woad-psi.vercel.app/api/user"
 })
 const feedBackApi = axios.create({
-    baseURL:"http://localhost:3000/api/feedback"
+    baseURL:"https://feedback-woad-psi.vercel.app/api/feedback"
 })
 
 export {authApi,userApi,feedBackApi};
